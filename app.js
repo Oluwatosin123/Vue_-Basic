4b31cbbdf7a23f31fc13cbb926d1b0770b934951
Ajose Tosin <tosinemidastouch@gmail.com>
2022-05-27 17:35:37 +0100

    Push basic tutorial on Vuejs to github

---
+++ app.js
@@ -0,0 +1,33 @@
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector("ul");
+
+// function addList() {
+//     const enteredValue = inputEl.value;
+//     const  listItemEl = document.createElement('li');
+//     listItemEl.textContent = enteredValue;
+//     listEl.appendChild( listItemEl);
+//     inputEl.value = " ";
+// }
+
+// buttonEl.addEventListener('click', addList);
+
+Vue.createApp({
+    // data: function() {
+
+    // }
+
+    data() {
+        return{
+            goals: [],
+            enteredValue: ''
+        };
+    },
+
+    methods: {
+        addGoal () {
+            this.goals.push(this.enteredValue);
+            this.enteredValue = "";
+        }
+    }
+}).mount('#app');
